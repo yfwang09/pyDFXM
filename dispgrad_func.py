Fixed the rotation matrix for edge dislocation case

dispgrad_func.py
@@ -192,30 +192,30 @@ class edge_disl(dispgrad_structure):
 
         errorlist = []
         # Burger's vector
-        if 'bs' in d:
-            bs = d['bs']
-            self.bs = np.divide(bs, np.linalg.norm(bs))
+        if 'bg' in d:
+            bs = d['bg']
+            self.bg = np.divide(bs, np.linalg.norm(bs))
         else:
-            errorlist.append('bs')
+            errorlist.append('bg')
         # normal vector of the slip plane
-        if 'ns' in d:
-            ns = d['ns']
-            self.ns = np.divide(ns, np.linalg.norm(ns))
+        if 'ng' in d:
+            ns = d['ng']
+            self.ng = np.divide(ns, np.linalg.norm(ns))
         else:
-            errorlist.append('ns')
+            errorlist.append('ng')
         # dislocation line direction
-        if 'ts' in d:
-            ts = d['ts']
-            self.ts = np.divide(ts, np.linalg.norm(ts))
+        if 'tg' in d:
+            ts = d['tg']
+            self.tg = np.divide(ts, np.linalg.norm(ts))
         else:
-            errorlist.append('ts')
+            errorlist.append('tg')
 
         if len(errorlist) > 0:
             errorstr = ', '.join(errorlist)
             raise ValueError("edge_disl.__init__(): please define %s in d:dict"%errorstr)
 
         # get the rotation matrix for the dislocation coordinates
-        self.Ud = return_dis_grain_matrices(b=self.bs, n=self.ns, t=self.ts) # shape (3, 3)
+        self.Ud = return_dis_grain_matrices(b=self.bg, n=self.ng, t=self.tg) # shape (3, 3)
 
         # Define grain rotation Ug (done in dispgrad_structure.__init__())
 

test_dispgrad_edge.py
@@ -1,233 +1,266 @@
+perform_test = [3, ]
+
 # This code tests the the displacement gradient field of a single edge dislocation
 
 print('------- Test 1: Compute the rotation matrix from function --------')
 
-from matplotlib import tight_layout
-import numpy as np
-import dispgrad_func as dgf
+if 1 not in perform_test:
+    print('Test 1 skipped')
+else:
 
-be = [1,-1, 0]
-ns = [1, 1,-1]
-xi = [1, 1, 2]
+    from matplotlib import tight_layout
+    import numpy as np
+    import dispgrad_func as dgf
 
-print('Burgers vector', be)
-print('Normal vector', ns)
-print('Line direction', xi)
-print()
+    be = [1,-1, 0]
+    ns = [1, 1,-1]
+    xi = [1, 1, 2]
 
-# Calculate the rotation matrix based on the normal and dislocation line directions (equation 65 in Poulsen et al., 2021)
-print('Ud from return_dis_grain_matrices')
-Ud = dgf.return_dis_grain_matrices(b=be, n=ns, t=xi)
-print(Ud)
+    print('Burgers vector', be)
+    print('Normal vector', ns)
+    print('Line direction', xi)
+    print()
 
-# Compare with the original all matrices in the all 12 slip systems (transposed)
-print('Ud from slip system database')
-Udall = dgf.return_dis_grain_matrices_all()
-print(Udall[:, :, 3].T)
+    # Calculate the rotation matrix based on the normal and dislocation line directions (equation 65 in Poulsen et al., 2021)
+    print('Ud from return_dis_grain_matrices')
+    Ud = dgf.return_dis_grain_matrices(b=be, n=ns, t=xi)
+    print(Ud)
 
-test_pass = np.linalg.norm((Udall[:, :, 3].T - Ud).flatten()) < 1e-7
-print('Test passed?', test_pass)
+    # Compare with the original all matrices in the all 12 slip systems (transposed)
+    print('Ud from slip system database')
+    Udall = dgf.return_dis_grain_matrices_all()
+    print(Udall[:, :, 3].T)
+
+    test_pass = np.linalg.norm((Udall[:, :, 3].T - Ud).flatten()) < 1e-7
+    print('Test passed?', test_pass)
 
 print('------------------------------------------------------------------')
 
 print('-------- Test 2: 2D strain field of an edge dislocation ----------')
 
-import numpy as np
-import matplotlib.pyplot as plt
-import dispgrad_func as dgf
+if 2 not in perform_test:
+    print('Test 2 skipped')
+else:
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import dispgrad_func as dgf
 
-print('Create the strain field of an edge dislocation')
+    print('Create the strain field of an edge dislocation')
 
-xd = np.linspace(-1, 1, 100)
-yd = np.linspace(-1, 1, 100)
-input_dict = {
-    'b': 1, 'nu': 0.334,
-    'bs': [1,-1, 0], # Burger's vector dir. in Miller (sample coord)
-    'ns': [1, 1,-1], # Normal vector dir. in Miller (sample coord)
-    'ts': [1, 1, 2], # Dislocation line dir. in Miller (sample coord)
-}
-print(input_dict)
+    xd = np.linspace(-1, 1, 100)
+    yd = np.linspace(-1, 1, 100)
+    input_dict = {
+        'b': 1, 'nu': 0.334,
+        'bg': [1,-1, 0], # Burger's vector dir. in Miller (grain coord)
+        'ng': [1, 1,-1], # Normal vector dir. in Miller (grain coord)
+        'tg': [1, 1, 2], # Dislocation line dir. in Miller (grain coord)
+    }
+    print(input_dict)
 
-edge = dgf.edge_disl(input_dict)
+    edge = dgf.edge_disl(input_dict)
 
-XX, YY = np.meshgrid(xd, yd)
-Hd = edge.get_disl_strain_tensor(XX, YY) - np.identity(3)
-print('Fd - I = Hd.shape =', Hd.shape)
+    XX, YY = np.meshgrid(xd, yd)
+    Hd = edge.get_disl_strain_tensor(XX, YY) - np.identity(3)
+    print('Fd - I = Hd.shape =', Hd.shape)
 
-subs = ['x', 'y', 'z']
-strain = (Hd + np.swapaxes(Hd, -1, -2))/2
-print('Visualize the strain tensor in plane')
+    subs = ['x', 'y', 'z']
+    strain = (Hd + np.swapaxes(Hd, -1, -2))/2
+    print('Visualize the strain tensor in plane')
 
-fs = 12
-vmin, vmax = -0.4, 0.4
-fig, axs = plt.subplots(2, 2, figsize=(6, 5), 
-                        sharex=True, sharey=True, )
-for i in range(2):
-    for j in range(2):
-        ax = axs[i][j]
-        cim = ax.imshow(Hd[..., i,j], extent=[-1,1,-1,1], vmin=vmin, vmax=vmax)
-        if i == 1:
-            ax.set_xlabel('x/b', fontsize=fs)
-        if j == 0:
-            ax.set_ylabel('y/b', fontsize=fs)
-        ax.set_title(r'$H^d_{%s%s}$'%(subs[i], subs[j]), fontsize=fs)
+    fs = 12
+    vmin, vmax = -0.4, 0.4
+    fig, axs = plt.subplots(2, 2, figsize=(6, 5), 
+                            sharex=True, sharey=True, )
+    for i in range(2):
+        for j in range(2):
+            ax = axs[i][j]
+            cim = ax.imshow(Hd[..., i,j], extent=[-1,1,-1,1], vmin=vmin, vmax=vmax)
+            if i == 1:
+                ax.set_xlabel('x/b', fontsize=fs)
+            if j == 0:
+                ax.set_ylabel('y/b', fontsize=fs)
+            ax.set_title(r'$H^d_{%s%s}$'%(subs[i], subs[j]), fontsize=fs)
 
-fig.colorbar(cim, ax=axs)
-fig.suptitle(r'Dislocation coordinate system $\mathbf{H}^d=\mathbf{F}^d-\mathbf{I}$')
-plt.show()
+    fig.colorbar(cim, ax=axs)
+    fig.suptitle(r'Dislocation coordinate system $\mathbf{H}^d=\mathbf{F}^d-\mathbf{I}$')
+    plt.show()
 
 print('------------------------------------------------------------------')
-
 print('-------- Test 3: 3D strain field of an edge dislocation ----------')
 
-import numpy as np
-import matplotlib.pyplot as plt
-import dispgrad_func as dgf
-import visualize_helper as vis
+if 3 not in perform_test:
+    print('Test 3 skipped')
+else:
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import dispgrad_func as dgf
+    import forward_model as fwd
+    import visualize_helper as vis
 
-print('Set up of the dislocation structure')
-input_dict = {'b': 1, 'nu': 0.334, 
-              # The following defines the grain rotation (Ug, Eq.7-8)
-            #   'hkl': [0,0,1], # hkl diffraction plane, z dir. crystal
-            #   'x_c': [1,0,0], # x dir. for the crystal system (Fig.2)
-            #   'y_c': [0,1,0], # y dir. for the crystal system (Fig.2)
-              'hkl' : [1,1,1],
-              'x_c' : [1,1,-2],
-              'y_c' : [-1,1,0],
-              # The following defines the dislocation structure
-              'bs': [1,-1, 0], # Burger's vector dir. in Miller (sample)
-              'ns': [1, 1,-1], # Normal vector dir. in Miller (sample)
-              'ts': [1, 1, 2], # Dislocation line dir. in Miller (sample)
-             }
-print(input_dict)
-edge = dgf.edge_disl(input_dict)
-Ud = edge.Ud
-Ug = edge.Ug
-print('Ud =')
-print(Ud)
-print('Ug =')
-print(Ug)
+    print('Set up of the dislocation structure')
+    # The dispgrad_func class always assuming the structure is aligned with Miller's indices.
+    input_dict = {
+        'b': 1, 'nu': 0.334,
+        # The following defines the dislocation characters
+        'bg': [1,-1, 0], # Burger's vector dir. in Miller (grain)
+        'ng': [1, 1,-1], # Normal vector dir. in Miller (grain)
+        'tg': [1, 1, 2], # Dislocation line dir. in Miller (grain)
+    }
+    print(input_dict)
+    edge = dgf.edge_disl(input_dict)
+    Ud = edge.Ud
+    print('Ud =')
+    print(Ud)
 
-print('Define the voxelized 3D field')
+    forward_dict = fwd.default_forward_dict()
+    forward_dict['b'] = 1
+    # The following defines the sample coordinate (Ug, Ug = U.T, Eq.7-8)
+    forward_dict['x_c'] = [1,0,0]  # x dir. for the crystal system (Fig.2)
+    forward_dict['y_c'] = [0,1,0]  # y dir. for the crystal system (Fig.2)
+    forward_dict['hkl'] = [0,0,1]  # hkl diffraction plane, z dir. crystal
+    # forward_dict['hkl'] = [1, 1, 1]
+    # forward_dict['x_c'] = [1, 1,-2]
+    # forward_dict['y_c'] = [-1,1, 0]
+    forward_dict['hkl'] = [1, 1,-1]
+    forward_dict['x_c'] = [1, 1, 2]
+    forward_dict['y_c'] = [1,-1, 0]
+    model = fwd.DFXM_forward(forward_dict, load_res_fn=None)
+    Ug = model.Ug
 
-lb, ub = -1, 1
-Ngrid = 50
-offset = 1e-4                           # to avoid the singularity at the core
-xg = np.linspace(lb, ub, Ngrid)-offset  # (Ngrid, )
-yg = np.linspace(lb, ub, Ngrid)         # (Ngrid, )
-zg = np.linspace(lb, ub, Ngrid)         # (Ngrid, )
+    print('Define the voxelized 3D field')
 
-XX, YY, ZZ = np.meshgrid(xg, yg, zg)    # (Ngrid, Ngrid, Ngrid)
-Rg = np.stack([XX,YY,ZZ], -1)           # (Ngrid, Ngrid, Ngird, 3)
-print('Grid size in the grain system:', Rg.shape)
+    lb, ub = -1, 1
+    Ngrid = 50
+    offset = 1e-4                           # to avoid the singularity at the core
+    xs = np.linspace(lb, ub, Ngrid)-offset  # (Ngrid, )
+    ys = np.linspace(lb, ub, Ngrid)         # (Ngrid, )
+    zs = np.linspace(lb, ub, Ngrid)         # (Ngrid, )
 
-print('Convert Rg into sample system (Miller indices)')
-RR = np.einsum('ij,...j->...i', np.linalg.inv(Ug), Rg)
+    XX, YY, ZZ = np.meshgrid(xs, ys, zs)    # (Ngrid, Ngrid, Ngrid)
+    Rs = np.stack([XX,YY,ZZ], -1)           # (Ngrid, Ngrid, Ngird, 3)
+    print('Grid size in the sample coordinates:', Rs.shape)
 
-print('Rotate the grid into the dislocation coordinate')
-Rd = np.sum(np.linalg.inv(Ud)*RR[:,:,:,None,:], axis=-1)    # (Ngrid, Ngrid, Ngrid, 3)
-Rdx = Rd[..., 0]
-Rdy = Rd[..., 1]
+    print('Convert Rs into the grain coordinates (Miller indices)')
+    Rg = np.einsum('ij,...j->...i', Ug, Rs)
 
-print('2D dislocation coordinate:', Rdx.shape, Rdy.shape)
+    print('Rotate the grid into the dislocation coordinate')
+    Rd = np.einsum('ij,...j->...i', np.linalg.inv(Ud), Rg)    # (Ngrid, Ngrid, Ngrid, 3)
+    # Rd = np.sum(np.linalg.inv(Ud)*RR[:,:,:,None,:], axis=-1)
+    Rdx = Rd[..., 0]
+    Rdy = Rd[..., 1]
 
-print('Calculate the strain tensor')
-Fd = edge.get_disl_strain_tensor(Rdx, Rdy)      # (Ngrid, Ngrid, Ngrid, 3, 3)
-print('Strain tensor:', Fd.shape)
+    print('2D dislocation coordinate:', Rdx.shape, Rdy.shape)
 
-print('Convert back to the sample coordinate (Miller)')
-Fs = np.einsum('ij,...jk,kl->...il', Ud, Fd, Ud.T)         # (Ngrid, Ngrid, Ngrid, 3, 3)
-print('Fs.shape =', Fs.shape)
+    print('Calculate the strain tensor')
+    Fd = edge.get_disl_strain_tensor(Rdx, Rdy)      # (Ngrid, Ngrid, Ngrid, 3, 3)
+    print('Strain tensor:', Fd.shape)
 
-print('Convert back to the grain coordinate')
-Fg = np.einsum('ij,...jk,kl->...il', Ug, Fs, Ug.T)         # (Ngrid, Ngrid, Ngrid, 3, 3)
-print('Fg.shape =', Fg.shape)
+    print('Convert back to the grain coordinate (Miller)')
+    Fg = np.einsum('ij,...jk,kl->...il', Ud, Fd, Ud.T)         # (Ngrid, Ngrid, Ngrid, 3, 3)
+    print('Fg.shape =', Fg.shape)
 
-print('Visualize the Hg tensor as 2D slices')
+    print('Convert back to the sample coordinate')
+    Fs = np.einsum('ij,...jk,kl->...il', Ug.T, Fg, Ug)         # (Ngrid, Ngrid, Ngrid, 3, 3)
+    print('Fs.shape =', Fs.shape)
 
-subs = ['x', 'y', 'z']
-vmin, vmax = -1, 1
-Hg = Fg - np.identity(3)
+    print('Visualize the Fs tensor as 2D slices')
 
-for iz in np.linspace(0, Ngrid - 1, 9).round().astype(int):
-    fig, axs = plt.subplots(3, 3, figsize=(6, 4),
-                            sharex=True, sharey=True)
+    subs = ['x', 'y', 'z']
+    vmin, vmax = -1, 1
+    F_plot = Fs - np.identity(3)
+
+    fs = 12
+    vmin, vmax = -1, 1
+    for iz in np.linspace(0, Ngrid - 1, 9).round().astype(int):
+        fig, axs = plt.subplots(3, 3, figsize=(6, 4),
+                                sharex=True, sharey=True)
+        for i in range(3):
+            for j in range(3):
+                F_z = F_plot[::-1, :, iz, i, j]
+                ax = axs[i][j]
+                im = ax.imshow(F_z, extent=[lb, ub, lb, ub], vmin=vmin, vmax=vmax)
+                ax.set_title(r' $H^g_{%s%s}$'%(subs[i], subs[j]), loc='left', y=0.6, color='w')
+            axs[i][0].set_ylabel(r'$y^s/b$')
+            axs[-1][i].set_xlabel(r'$x^s/b$')
+        fig.colorbar(im, ax=axs)
+        fig.suptitle(r'Grain coordinate system, $z^s/b$=%.4f'%zs[iz], fontsize=fs)
+        print('  visualizing the slice zs = %.4f'%zs[iz])
+    plt.show()
+
+    print('Test the visualization helper function plot_2d_slice_z()')
+    figax = vis.plot_2d_slice_z(F_plot[:, :, :, 1, 2], vmin=vmin, vmax=vmax, show=False)
+    figax[0].suptitle(r'Grain coordinate system, $H^s_{yz}$', fontsize=fs)
+    plt.show()
+
+    print('Visualize 3D slices of each component in Hg')
+
+    from matplotlib.colors import Normalize
+    subs = ['x', 'y', 'z']
+
+    print('Define the x,y coordinates of the slices')
+    xx, yy = np.meshgrid(xs, ys)
+    zz = (-edge.ng[0] * xx - edge.ng[1] * yy) / edge.ng[2]
+    zz[zz>ub] = np.nan
+    zz[zz<lb] = np.nan
+    vmin, vmax = -1, 1
+    fs = 12
+
+    print('Define the x,y,z coordinates of the dislocation line')
+    ztg = np.linspace(lb, ub)
+    ytg = ztg/edge.tg[2]*edge.tg[1]
+    xtg = ztg/edge.tg[2]*edge.tg[0]
+    # Convert from grain to sample coordinates
+    rts = np.einsum('ij,...j->...i', (Ug.T), np.transpose([xtg, ytg, ztg]))
+    xt, yt, zt = rts[..., 0], rts[..., 1], rts[..., 2]
+
+    print('Visualize the 3D slices')
     for i in range(3):
         for j in range(3):
-            Fg_z = Hg[::-1, :, iz, i, j]
-            ax = axs[i][j]
-            im = ax.imshow(Fg_z, extent=[lb, ub, lb, ub], vmin=vmin, vmax=vmax)
-            ax.set_title(r' $H^g_{%s%s}$'%(subs[i], subs[j]), loc='left', y=0.6, color='w')
-        axs[i][0].set_ylabel(r'$y^g/b$')
-        axs[-1][i].set_xlabel(r'$x^g/b$')
-    fig.colorbar(im, ax=axs)
-    fig.suptitle(r'Grain coordinate system, $z^g/b$=%.4f'%zg[iz], fontsize=fs)
-    print('  visualizing the slice zg = %.4f'%zg[iz])
-plt.show()
+            print('  visualizing %s%s component'%(subs[i], subs[j]))
+            fig = plt.figure(figsize=(6, 4))
+            ax = fig.add_subplot(111, projection='3d')
+            ax.plot([lb, lb, ub, ub], [ub, ub, ub, ub], [lb, ub, ub, lb], 'k')
+            ax.plot([lb, lb, ub, ub], [lb, ub, ub, lb], [lb, lb, lb, lb], 'k')
+            ax.plot(xt, yt, zt, 'k', linewidth=2)
 
-print('Test the visualization helper function plot_2d_slice_z()')
-figax = vis.plot_2d_slice_z(Hg[:, :, :, 0, 2], vmin=vmin, vmax=vmax, show=False)
-figax[0].suptitle(r'Grain coordinate system, $H^g_{xz}$', fontsize=fs)
-plt.show()
+            for iz in range(5, 50, 10):
+                zloc = zs[iz]                  # z location of the plane
+                Fg_z = F_plot[:, :, iz, i, j]      # value of the plane
+                zval = np.clip(Fg_z, vmin, vmax)
+                # print(zval.min(), zval.max())
+                norm = Normalize(vmin=vmin, vmax=vmax)
+                surf = ax.plot_surface(xx, yy, np.ones_like(xx)*zloc, linewidth=0, edgecolor="None", facecolors=plt.cm.viridis(norm(zval)), alpha=0.5)
+                
+            cax = fig.colorbar(surf, shrink=0.5)
+            cticks = cax.get_ticks()  # Always in the range of [0, 1]
+            cax.set_ticks(cticks)
+            cticklabels = (cticks-cticks.min())/(cticks.max()-cticks.min())*(vmax-vmin) + vmin
+            cax.set_ticklabels(['%.1f'%k for k in cticklabels])
+            ax.set_title(r'Sample coordinate system $H^s_{%s%s}$'%(subs[i], subs[j]), fontsize=fs)
 
-print('Visualize 3D slices of each component in Hg')
+            ax.plot([lb, lb, ub, ub], [lb, lb, lb, lb], [ub, lb, lb, ub], 'k')
+            ax.plot([lb, lb, ub, ub], [ub, lb, lb, ub], [ub, ub, ub, ub], 'k')
+            ax.set_xlabel(r'$x^s/b$', fontsize=fs)
+            ax.set_ylabel(r'$y^s/b$', fontsize=fs)
+            ax.set_zlabel(r'$z^s/b$', fontsize=fs)
+    plt.show()
 
-from matplotlib.colors import Normalize
-subs = ['x', 'y', 'z']
-
-print('Define the x,y coordinates of the slices')
-xx, yy = np.meshgrid(xg, yg)
-zz = (-ns[0] * xx - ns[1] * yy) / ns[2]
-zz[zz>ub] = np.nan
-zz[zz<lb] = np.nan
-vmin, vmax = -0.4, 0.4
-fs = 12
-
-print('Define the x,y,z coordinates of the dislocation line')
-zt = np.linspace(lb, ub)
-yt = zt/edge.ts[2]*edge.ts[1]
-xt = zt/edge.ts[2]*edge.ts[0]
-# In the grain system
-rtg = np.einsum('ij,...j->...i', (Ug), np.transpose([xt, yt, zt]))
-xt, yt, zt = rtg[..., 0], rtg[..., 1], rtg[..., 2]
-
-print('Visualize the 3D slices')
-for i in range(3):
-    for j in range(3):
-        print('  visualizing %s%s component'%(subs[i], subs[j]))
-        fig = plt.figure(figsize=(6, 4))
-        ax = fig.add_subplot(111, projection='3d')
-        ax.plot([lb, lb, ub, ub], [ub, ub, ub, ub], [lb, ub, ub, lb], 'k')
-        ax.plot([lb, lb, ub, ub], [lb, ub, ub, lb], [lb, lb, lb, lb], 'k')
-        ax.plot(xt, yt, zt, 'k', linewidth=2)
-        
-        for iz in range(5, 50, 10):
-            zloc = zg[iz]                  # z location of the plane
-            Fg_z = Hg[:, :, iz, i, j]      # value of the plane
-            zval = np.clip(Fg_z, vmin, vmax)
-            # print(zval.min(), zval.max())
-            norm = Normalize(vmin=vmin, vmax=vmax)
-            surf = ax.plot_surface(xx, yy, np.ones_like(xx)*zloc, linewidth=0, edgecolor="None", facecolors=plt.cm.viridis(norm(zval)), alpha=0.5)
-            
-        cax = fig.colorbar(surf, shrink=0.5)
-        cticks = cax.get_ticks()  # Always in the range of [0, 1]
-        cax.set_ticks(cticks)
-        cticklabels = (cticks-cticks.min())/(cticks.max()-cticks.min())*(vmax-vmin) + vmin
-        cax.set_ticklabels(['%.1f'%k for k in cticklabels])
-        ax.set_title(r'Grain coordinate system $H^g_{%s%s}$'%(subs[i], subs[j]), fontsize=fs)
-
-        ax.plot([lb, lb, ub, ub], [lb, lb, lb, lb], [ub, lb, lb, ub], 'k')
-        ax.plot([lb, lb, ub, ub], [ub, lb, lb, ub], [ub, ub, ub, ub], 'k')
-        ax.set_xlabel(r'$x^g/b$', fontsize=fs)
-        ax.set_ylabel(r'$y^g/b$', fontsize=fs)
-        ax.set_zlabel(r'$z^g/b$', fontsize=fs)
-plt.show()
-
-print('Test the visualization helper function plot_3d_slice_z')
-figax = vis.plot_3d_slice_z(Hg[:, :, :, 0, 2], extent=[lb, ub, lb, ub, lb, ub], vmin=vmin, vmax=vmax, nslice=5, fs=fs, show=False)
-fig, ax = figax
-ax.plot(xt, yt, zt, 'k', lw=2)
-ax.set_title(r'Grain coordinate system, $H^g_{xz}$', fontsize=fs)
-plt.show()
+    print('Test the visualization helper function plot_3d_slice_z')
+    figax = vis.plot_3d_slice_z(F_plot[:, :, :, 1, 2], extent=[lb, ub, lb, ub, lb, ub], vmin=vmin, vmax=vmax, nslice=5, fs=fs, show=False)
+    fig, ax = figax
+    ax.plot(xt, yt, zt, 'k', lw=2)
+    ax.set_title(r'Grain coordinate system, $H^s_{yz}$', fontsize=fs)
+    plt.show()
 
 print('------------------------------------------------------------------')
+
+# forward_dict = {
+#     # The following defines the grain rotation (Ug, Eq.7-8)
+#     # 'hkl': [0,0,1], # hkl diffraction plane, z dir. crystal
+#     # 'x_c': [1,0,0], # x dir. for the crystal system (Fig.2)
+#     # 'y_c': [0,1,0], # y dir. for the crystal system (Fig.2)
+#     'hkl' : [1,1,1],
+#     'x_c' : [1,1,-2],
+#     'y_c' : [-1,1,0],
+
+# }