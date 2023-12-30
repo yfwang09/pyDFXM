b9a41b9efe864a4b2499776f308e8c0e49107856
YF Wang <yfwang09@stanford.edu>
2023-12-30 00:42:18 -0800

    update visualization code for Al case

--- test_Al_10um_edge_pbc_DFXM.py
+++ test_Al_10um_edge_pbc_DFXM.py
@@ -77,16 +77,7 @@ vmin, vmax = -1e-4, 1e-4
 fs = 12
 extent = np.multiply(1e6, [lb, ub, lb, ub, lb, ub]) # in the unit of um
 figax = vis.plot_3d_slice_z(Fg[:, :, :, i, j], extent=extent, vmin=vmin, vmax=vmax, nslice=5, fs=fs, show=False)
-fig, ax = figax
-for i in range(disl.links.shape[0]):
-    n12 = disl.links[i, 0:2].astype(int)
-    r12 = disl.rn[n12, :]*bmag*1e6 # in unit of um
-    ax.plot(r12[..., 0], r12[..., 1], r12[..., 2],  'ko-')
-
-ax.set_title(r'Grain coordinate system, $F^g_{yz}$', fontsize=fs)
-ax.set_xlabel(r'${\rm x(\mu m)}$')
-ax.set_ylabel(r'${\rm y(\mu m)}$')
-ax.set_zlabel(r'${\rm z(\mu m)}$')
+figax = vis.visualize_disl_network(disl.d, disl.rn, disl.links, unit='um', figax=figax, show=False)
 plt.show()
 
 #%%-------------------------------------------------------
@@ -106,21 +97,10 @@ figax = vis.visualize_im_qi(forward_dict, im, None, rulers) #, vlim_im=[0, 200])
 # figax = vis.visualize_im_qi(forward_dict, None, ql, rulers, vlim_qi=[-1e-4, 1e-4])
 
 # Visualize the observation points
-uc = 1e6 # um/m
-r_obs = np.load(saved_Fg_file)['r_obs'] * uc
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-lb, ub = -L/2*bmag * uc, L/2*bmag * uc
-ax.plot([lb, lb, ub, ub], [ub, ub, ub, ub], [lb, ub, ub, lb], 'k')
-ax.plot([lb, lb, ub, ub], [lb, ub, ub, lb], [lb, lb, lb, lb], 'k')
-for i in range(disl.links.shape[0]):
-    n12 = disl.links[i, 0:2].astype(int)
-    r12 = disl.rn[n12, :]*bmag * uc
-    ax.plot(r12[..., 0], r12[..., 1], r12[..., 2],  'C3o-')
-
+lb, ub = -L/2, L/2                                  # in unit of b
+extent = np.multiply(bmag*1e6, [lb, ub, lb, ub, lb, ub]) # in the unit of um
+fig, ax = vis.visualize_disl_network(disl.d, disl.rn, disl.links, extent=extent, unit='um', show=False)
 nskip = 10
+r_obs = np.load(saved_Fg_file)['r_obs']*1e6 # in the unit of um
 ax.plot(r_obs[::nskip, 0], r_obs[::nskip, 1], r_obs[::nskip, 2],  'C0.', markersize=0.01)
-ax.plot([lb, lb, ub, ub], [lb, lb, lb, lb], [ub, lb, lb, ub], 'k')
-ax.plot([lb, lb, ub, ub], [ub, lb, lb, ub], [ub, ub, ub, ub], 'k')
-ax.axis('equal')
 plt.show()
