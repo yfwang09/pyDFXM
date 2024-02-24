correct the rotation angle for lab-sample system

forward_model.py
@@ -78,7 +78,9 @@ class DFXM_forward():
                 else:
                     y_c = np.cross(z_c, x_c)
                     x_c = np.cross(y_c, z_c)
-                d['Ug'] = np.transpose([x_c, y_c, z_c])
+                # For definition of Ug = U in Eq. 5, U = [x_c^T, y_c^T, z_c^T]^T
+                # d['Ug'] = np.transpose([x_c, y_c, z_c])
+                d['Ug'] = np.array([x_c, y_c, z_c])
             else:
                 d['Ug'] = np.eye(3)
 
@@ -356,7 +358,11 @@ class DFXM_forward():
         # Determine the location of the pixel on the detector
         DET_IND_Y = np.round((YL-yl_start)/yl_step).astype(int)//Nsub # THIS ALIGNS WITH yl
         DET_IND_Z = np.round((XL0-xl_start)/xl_step).astype(int)//Nsub # THIS IS THE OTHER DETECTOR DIRECTION AND FOLLOWS xl BUT WITH MAGNIFICATION
-        RS = np.einsum('ji,...j->...i', Gamma, RL) # NB: Gamma inverse Eq. 5
+        #### This line is WRONG, the rotation is reversed ####
+        # RS = np.einsum('ji,...j->...i', Gamma, RL) # NB: Gamma inverse Eq. 5
+        #### The correct lines are ####
+        RS = np.einsum('ij,...j->...i', Gamma, RL) # Eq. 5
+        #########################################################
         RG = np.einsum('ji,...j->...i', U, RS)     # NB U inverse, Eq. 7
         Fg = Fg_fun(RG[..., 0], RG[..., 1], RG[..., 2]) # calculate the displacement gradient
 

test_single_dislocation_DFXM.py
@@ -42,8 +42,8 @@ bvec = [1, 0,-1]; nvec = [1, -1, 1]; rvec = [1, -1, 1]
 # bvec = [1, 0, 1]; nvec = [1, 1, -1]; rvec = [1, 1, -1]
 # bvec = [1, 0, 1]; nvec = [1, 1, -1]; rvec = [1,-1,-1]
 
-bvec = [1, 1, 0]; nvec = [1,-1, 1]; rvec = [0, 0, 1]
-# bvec = [1, 1, 0]; nvec = [1,-1, 1]; rvec = [1,-1, 1]
+# bvec = [1, 1, 0]; nvec = [1,-1, 1]; rvec = [0, 0, 1]
+bvec = [1, 1, 0]; nvec = [1,-1, 1]; rvec = [1,-1, 1]
 
 # bvec = [0, 1, 1]; nvec = [1, 1, -1]; rvec = [1, 1, -1]
 # bvec = [0, 1, 1]; nvec = [1, -1, 1]; rvec = [1, -1, 1]
@@ -268,7 +268,7 @@ if not os.path.exists(config_ca_file):
     ca_data = disl.write_network_ca(config_ca_file, bmag=bmag)
 
 r_obs_xyz_file = 'robs.xyz'
-saved_Fg_file = os.path.join('data', 'Fg_%s_DFXM_robs.npz'%casename)
+saved_Fg_file = os.path.join('data', 'Fg_%s_%s_DFXM_robs.npz'%(casename, savename))
 if not os.path.exists(r_obs_xyz_file):
     r_obs = np.load(saved_Fg_file)['r_obs']
     dio.write_xyz(r_obs_xyz_file, r_obs, scale=1e10)