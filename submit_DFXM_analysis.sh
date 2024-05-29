0df1905af8b8935ed1657da369c05d618f51a055
Yifan Wang <yfwang09@stanford.edu>
2024-05-28 22:31:33 -0700

    fixed bugs for saving im files

--- submit_DFXM_analysis.sh
+++ submit_DFXM_analysis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=dispgrad
-#SBATCH -n 28
+#SBATCH -n 30
 #SBATCH --mem 64000
 #SBATCH -t 7-00:00:00
 #SBATCH -p mc
@@ -8,17 +8,17 @@
 # Calculate disp grad using mpi4py
 ml system viz devel git python/3.9.0 py-numpy/1.20.3_py39 py-scipy/1.6.3_py39 py-matplotlib/3.4.2_py39 py-numba/0.54.1_py39 py-pandas/1.3.1_py39 gcc/10.1.0 openmpi/4.1.2 py-mpi4py/3.1.3_py39
 
-for case in diamond_MD0_200x100x100 diamond_MD20000_189x100x100 diamond_MD50000_174x101x100 diamond_MD100000_149x100x101 diamond_MD150000_131x100x104 diamond_MD200000_114x100x107
+for case in diamond_MD20000_189x100x100 diamond_MD50000_174x101x100 diamond_MD100000_149x100x101 diamond_MD150000_131x100x104 diamond_MD200000_114x100x107 diamond_MD0_200x100x100 
 do
-    python3 test_diamond_DFXM_workflow.py --casename ${case} --scale_cell 1.0 -hkl 111 &
-    python3 test_diamond_DFXM_workflow.py --casename ${case} --scale_cell 0.5 -hkl 004 
     for slip in 0 1 2 3 4 5 6
     do
         python3 test_diamond_DFXM_workflow.py --casename ${case} --scale_cell 1.0 --slip ${slip} -hkl 111 &
         python3 test_diamond_DFXM_workflow.py --casename ${case} --scale_cell 0.5 --slip ${slip} -hkl 111 &
         python3 test_diamond_DFXM_workflow.py --casename ${case} --scale_cell 1.0 --slip ${slip} -hkl 004 &
-        python3 test_diamond_DFXM_workflow.py --casename ${case} --scale_cell 0.5 --slip ${slip} -hkl 004 
+        python3 test_diamond_DFXM_workflow.py --casename ${case} --scale_cell 0.5 --slip ${slip} -hkl 004 &
     done
+    python3 test_diamond_DFXM_workflow.py --casename ${case} --scale_cell 1.0 -hkl 111 &
+    python3 test_diamond_DFXM_workflow.py --casename ${case} --scale_cell 0.5 -hkl 004
     wait
 done
 
--- test_diamond_DFXM_workflow.py
+++ test_diamond_DFXM_workflow.py
@@ -27,21 +27,27 @@ parser.add_argument('--rolling', '-chi', type=float, default=0, help='Rolling an
 parser.add_argument('--shift', '-sh', type=float, default=[0, 0, 0], nargs='+', help='Shift of the observation points (um)')
 parser.add_argument('--cutoff', '-c', type=float, default=0.51, help='Cutoff distance for the observation region (in scaled coordinates)')
 parser.add_argument('--slip', '-s', type=int, default=None, help='slip system')
-args = parser.parse_args()
+args, unknown = parser.parse_known_args()
 
 casename   = args.casename
 scale_cell = args.scale_cell
 phi, chi = args.rocking, args.rolling
 shift = args.shift
 cutoff = args.cutoff
+
+# For debugging purposes
+# casename = 'diamond_MD200000_114x100x107'
+# scale_cell = 0.5
+# args.slip = 0
+
 slipstr = ''
 if args.slip is not None:
     config_dir = os.path.join('configs', 'config_%s'%casename)
     slipstr = '_slip%d'%args.slip
-    sliptype_file = 'sliptype_%d.txt'%args.slip
-    with open(os.path.join(config_dir, sliptype_file), 'r') as f:
-        sliptype = f.readline().split()[1:].join('_')
-    select_seg_slip = np.loadtxt(os.path.join(config_dir, 'select_seg_%s.txt'%sliptype), dtype=int)
+    sliptype_file = os.path.join(config_dir, 'sliptype_%d.txt'%args.slip)
+    with open(sliptype_file, 'r') as f:
+        sliptype = '_'.join(f.readline().split()[1:])
+    select_seg_slip = np.loadtxt(sliptype_file, dtype=int)
 
 #%%
 # Define the elasticity properties of diamond
@@ -102,6 +108,7 @@ casename_scaled = casename + slipstr + '_scale%d'%(1/scale_cell)
 phi = chi = 0
 shift = [0, 0, 0]
 
+casename_noslip_scaled_hkl = casename + '_scale%d'%(1/scale_cell) + '_shift-%.2f-%.2f-%.2f'%tuple(shift) + '_hkl%d%d%d'%tuple(hkl)
 casename_scaled_hkl = casename_scaled + '_shift-%.2f-%.2f-%.2f'%tuple(shift) + '_hkl%d%d%d'%tuple(hkl)
 
 forward_dict = fwd.default_forward_dict()
@@ -122,6 +129,8 @@ os.makedirs(datapath, exist_ok=True)
 Fg_path = os.path.join(datapath, 'Fg_%s_seg'%casename)
 os.makedirs(Fg_path, exist_ok=True)
 im_path = os.path.join(datapath, 'im_%s'%casename)
+if args.slip is not None:
+    im_path = os.path.join(im_path, 'slip%d'%args.slip)
 os.makedirs(im_path, exist_ok=True)
 saved_res_fn = os.path.join(datapath, 'Res_qi_diamond_001.npz')
 print('saved resolution function at %s'%saved_res_fn)
@@ -163,12 +172,6 @@ NNxyz = tuple(NNxyz)
 print(NNxyz)
 r_obs = np.load(saved_Fg_file)['r_obs'] * 1e6 # in the unit of um
 r_obs_cell = np.swapaxes(np.reshape(r_obs, NNxyz + (3, ), order='F'), 1, 2)
-obs_cell = np.transpose([
-    r_obs_cell[-1, 0, 0, :] - r_obs_cell[0, 0, 0, :], 
-    r_obs_cell[0, -1, 0, :] - r_obs_cell[0, 0, 0, :],
-    r_obs_cell[0, 0, -1, :] - r_obs_cell[0, 0, 0, :]
-])
-print(obs_cell)
 
 ax.plot(r_obs_cell[:, 0, 0, 0], r_obs_cell[:, 0, 0, 1], r_obs_cell[:, 0, 0, 2], '-k')
 ax.plot(r_obs_cell[0, :, 0, 0], r_obs_cell[0, :, 0, 1], r_obs_cell[0, :, 0, 2], '-k')
@@ -188,10 +191,18 @@ ax.plot(r_obs[::nskip, 0], r_obs[::nskip, 1], r_obs[::nskip, 2],  'C0.', markers
 ax.view_init(azim=90, elev=0)
 plt.close()
 
+os.remove(saved_Fg_file)
+
 # %%-------------------------------------------------------
 # FILTER THE DISLOCATION SEGMENTS THAT ARE OUTSIDE THE OBSERVATION REGION
 #---------------------------------------------------------
 
+obs_cell = np.transpose([
+    r_obs_cell[-1, 0, 0, :] - r_obs_cell[0, 0, 0, :], 
+    r_obs_cell[0, -1, 0, :] - r_obs_cell[0, 0, 0, :],
+    r_obs_cell[0, 0, -1, :] - r_obs_cell[0, 0, 0, :]
+])*1e-6                      # in unit of m
+print(obs_cell)
 rn, links = load_disl_network(casename, scale_cell=scale_cell)
 nsegs = disl.links.shape[0]
 select_seg_inside = []
@@ -210,9 +221,10 @@ print('# of segments inside the observation region:', len(select_seg_inside))
 # -----------------------------------------------------------
 
 if args.slip is not None:
-    select_seg = np.intersect1d(select_seg_slip, select_seg_inside)
+    select_seg = np.intersect1d(select_seg_slip, select_seg_inside).tolist()
 else:
     select_seg = select_seg_inside
+print(len(select_seg))
 
 config_ca_inside_file = os.path.join('config_%s_inside.ca'%casename_scaled_hkl)
 rn, links = load_disl_network(casename, scale_cell=scale_cell, select_seg=select_seg, save_ca_file=config_ca_inside_file)
@@ -223,12 +235,12 @@ rn, links = load_disl_network(casename, scale_cell=scale_cell, select_seg=select
 
 print('#'*20 + ' Calculate and visualize the image')
 saved_Fg_file = os.path.join(datapath, 'Fg_%s_DFXM.npz'%casename_scaled_hkl)
-print('saved displacement gradient at %s'%saved_Fg_file)
+print('saved dispgrad at %s'%saved_Fg_file)
 
 if not os.path.exists(saved_Fg_file):
     Fg_saved = np.zeros(r_obs.shape + (3, ))
-    for iseg, seg in enumerate(select_seg_inside):
-        saved_Fg_seg_file = os.path.join(Fg_path, 'Fg_%s'%(casename_scaled_hkl, )) + '_iseg%d_DFXM.npz'%(seg, )
+    for iseg, seg in enumerate(select_seg):
+        saved_Fg_seg_file = os.path.join(Fg_path, 'Fg_%s'%(casename_noslip_scaled_hkl, )) + '_iseg%d_DFXM.npz'%(seg, )
         if os.path.exists(saved_Fg_seg_file):
             print('load %s'%saved_Fg_seg_file)
             Fg_seg = np.load(saved_Fg_seg_file)['Fg']
@@ -250,7 +262,7 @@ figax = vis.visualize_im_qi(forward_dict, im, None, rulers) #, vlim_im=[0, 200])
 # %%
 # save r_obs into xyz file
 
-r_obs_xyz_file = os.path.join(datapath, 'r_obs_%s.xyz'%casename_scaled_hkl)
+r_obs_xyz_file = os.path.join(datapath, 'r_obs_%s.xyz'%casename_noslip_scaled_hkl)
 im_Nobs = np.repeat(np.repeat(im, Nobs, axis=0), Nobs, axis=1)[:,:,np.newaxis]
 im_obs = np.tile(im_Nobs, (1, 1, model.d['Npixels'][2]*Nobs)).reshape((-1, 1))
 r_obs = r_obs_cell.reshape((-1, 3))
@@ -292,7 +304,6 @@ ax.set_xlabel(r'Rocking $\phi$ (rad)')
 ax.set_ylabel('Intensity (a.u.)')
 saved_rocking_curve = os.path.join(im_path, 'im_%s'%(casename_scaled)+'_hkl%d%d%d'%tuple(hkl)+'_rocking_DFXM.png')
 fig.savefig(saved_rocking_curve, dpi=300, transparent=True)
-# plt.close()
 plt.close()
 
 # %%
@@ -359,6 +370,9 @@ for iphi, phi in np.ndenumerate(PHI):
     saved_im_file = os.path.join(im_path, 'im_%s_DFXM.png'%casename_scaled_phi_chi_hkl)
     figax[0].savefig(saved_im_file, dpi=300, transparent=True)
     plt.close()
+    # os.makedirs(os.path.join(im_path, 'npz_data'))
+    saved_im_data = os.path.join(im_path, 'im_%s_DFXM.npz'%casename_scaled_phi_chi_hkl)
+    np.savez_compressed(saved_im_data, im=im, ql=ql, rulers=rulers)
 
     Imin[iphi], Imax[iphi], Iavg[iphi] = im.min(), im.max(), im.mean()
 
@@ -367,19 +381,22 @@ for iphi, phi in np.ndenumerate(PHI):
 
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
-ax.plot_surface(PHI, CHI, Iavg, cmap='viridis')
-ax.set_xlabel(r'$\phi$ (rad)')
-ax.set_ylabel(r'$\chi$ (rad)')
+ax.plot_surface(PHI*1000, CHI*1000, Iavg, cmap='viridis')
+ax.set_xlabel(r'$\phi$ ($\times10^3$ rad)')
+ax.set_ylabel(r'$\chi$ ($\times10^3$ rad)')
 ax.set_zlabel(r'$I_{\rm avg}$')
+lims = (np.min(np.stack([PHI, CHI]))*1000, np.max(np.stack([PHI, CHI]))*1000)
+ax.set_xlim(*lims)
+ax.set_ylim(*lims)
 saved_mosaic_space = os.path.join(im_path, 'im_%s'%(casename_scaled)+'_hkl%d%d%d'%tuple(hkl)+'_mosaic_3D.png')
 fig.savefig(saved_mosaic_space, dpi=300, transparent=True)
 plt.close()
 
 fig, ax = plt.subplots()
-ax.imshow(Iavg, cmap='viridis', extent=[phi_values.min(), phi_values.max(), chi_values.min(), chi_values.max()])
+ax.imshow(Iavg, cmap='viridis', extent=np.multiply(1000, [phi_values.min(), phi_values.max(), chi_values.min(), chi_values.max()]))
 ax.axis('equal')
-ax.set_xlabel(r'$\phi$ (rad)')
-ax.set_ylabel(r'$\chi$ (rad)')
+ax.set_xlabel(r'$\phi$ ($\times10^3$ rad)')
+ax.set_ylabel(r'$\chi$ ($\times10^3$ rad)')
 saved_mosaic_space = os.path.join(im_path, 'im_%s'%(casename_scaled)+'_hkl%d%d%d'%tuple(hkl)+'_mosaic_2D.png')
 fig.savefig(saved_mosaic_space, dpi=300, transparent=True)
 plt.close()
